e0e54e8ba33b3adfb219273f1cac0995f2ac9452
AThreeDimensionalLemon <eisigliang@gmail.com>
2026-05-15 17:12:59 -0700

    Added tilesets to canvas

--- src/Scenes/Level1.js
+++ src/Scenes/Level1.js
@@ -5,10 +5,29 @@ class Level1 extends Phaser.Scene {
 
     preload() {
 
+        //queue to-be-loaded map assets
+        this.load.setPath("./assets/maps/");
+        this.load.image("spritesheet_terrain_spring");
+        this.load.image("spritesheet_terrain_spring");
+        this.load.image("spritesheet_terrain_fall");
+        this.load.image("spritesheet_detail_fall");
+        this.load.image("spritesheet_terrain_winter");
+        this.load.image("spritesheet_detail_winter");
+        this.load.tilemapTiledJSON("set_spring");
+        this.load.tilemapTiledJSON("set_summer");
+        this.load.tilemapTiledJSON("set_fall");
+        this.load.tilemapTiledJSON("set_winter");
     }
 
     create() {
 
+//create map sets
+        this.sets = [
+            this.add.tilemap("set_spring"),
+            this.add.tilemap("set_summer"),
+            this.add.tilemap("set_fall"),
+            this.add.tilemap("set_winter")
+        ];
     }
 
     update() {
